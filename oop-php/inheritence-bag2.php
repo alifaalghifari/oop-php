35b72d3807e4ff96ffa22ba696815d219608ea88
MuhAlifAlghifari <alghalif056@gmail.com>
2020-03-14 17:14:27 +0800

    membuat file inheritence-bag2.php

---
+++ oop-php/inheritence-bag2.php
@@ -0,0 +1,71 @@
+<?php 
+
+class Produk{
+	public $judul, $penulis, $penerbit, $harga, $jmlHalaman, $lamaMain;
+
+	// constructor 
+	public function __construct($judul="judul",$penulis="penulis",$penerbit="penerbit",$harga=0,$jmlHalaman=0,$lamaMain=0){
+		$this->judul = $judul;
+		$this->penulis = $penulis;
+		$this->penerbit = $penerbit;
+		$this->harga = $harga;
+		$this->jmlHalaman = $jmlHalaman;
+		$this->lamaMain = $lamaMain;
+	}
+
+	public function getLabel(){
+		return $this->penulis .", ". $this->penerbit;
+	}
+
+	public function infoProduk(){
+		
+		$str = "{$this->judul} | {$this->getLabel()}, Rp {$this->harga} ";
+		return $str;
+	}
+
+}
+
+class Buku extends Produk{
+	public function infoProduk(){
+		$str = "Buku : {$this->judul} | {$this->getLabel()}, Rp {$this->harga} - {$this->jmlHalaman} Halaman";
+		return $str;
+	}
+}
+
+class Game extends Produk{
+	public function infoProduk(){
+		$str = "Game : {$this->judul} | {$this->getLabel()}, Rp {$this->harga} - {$this->lamaMain} Jam";
+		return $str;
+	}
+}
+
+class cetakInfoProduk{
+	public function info(Produk $produk){
+		return "{$produk->judul} | {$produk->getLabel()}, Rp {$produk->harga} ";
+	}
+
+		public function infoLengkap(Produk $produk){
+		
+		$str = "{$produk->judul} | {$produk->getLabel()}, Rp {$produk->harga} ";
+		return $str;
+	}
+}
+
+
+
+$produk1 = new Buku("Seni hidup minimalis","Francine jay","Gramedia",35000,100,0);
+$produk2  = new Game("Uncharted","Neil Druckman","Sony Computer",200000,0,50);
+
+
+echo $produk1->infoProduk();
+echo "<br>";
+echo $produk2->infoProduk();
+
+
+
+
+
+
+
+
+
